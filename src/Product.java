d81c7ead308c56761a1bc5230448ae846dd32e75
tkiguess <ahmedtafkir@gmail.com>
2018-07-25 15:39:13 -0400

    update

--- src/Product.java
+++ src/Product.java
@@ -7,10 +7,36 @@ public class Product {
     private Double price;
     protected static int count = 0; // a protected static variable
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
     public Product() {
         code = "";
         description = "";
         price = 0.0;
+
+
     }
 
     // Get and set accessors for the code, description, and price instance variables
@@ -21,6 +47,7 @@ public class Product {
                 "Description:        " + description + "\n" +
                 "Price:              " + this.getFormattedPrice(price) + "\n";
     }
+
 
     private String getFormattedPrice(double unformat) {
         String formattedData = String.format("%.02f", unformat);
--- src/beefyboi.java
+++ src/beefyboi.java
@@ -22,9 +22,21 @@ public class beefyboi {
 
         System.out.println("=======================");
 
+        System.out.println("Code: ");
+        String load = keyboard.next();
+        p.setCode(load);
+
+        System.out.println("description: ");
+        String prescription = keyboard.next();
+        p.setDescription(prescription);
+
+        System.out.println("Price");
+        Double rice = keyboard.nextDouble();
+        p.setPrice(rice);
+
         System.out.print(p.toString());
 
-
+        System.out.println("I understard the rest and how it works.");
     }
 
 }
